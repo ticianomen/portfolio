e00f5dd8d8997dde3798ad57ccb85c19c830fa54
Luciano <pereiraluciano169@gmail.com>
2021-09-13 17:12:20 -0300

    created navbar component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Cover from './components/cover/Cover.jsx';
+import Navbar from './components/navbar/Navbar';
 
 function App() {
+  const [scroll, setScroll] = useState(0);
+
+  const handleScroll = () => {
+    let position = window.pageYOffset; //toammos la posicion del scroll
+    setScroll(position); //la guardamos
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll) //disparamos el evento con la funcion creada
+  }, [scroll])
+
   return (
     <div className="App">
+      <Navbar isScrolling={scroll} />
       <Cover />
     </div>
   );
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Navbar.css';
+
+const Navbar = ({ isScrolling }) => {
+  const toTop = () => {
+    //le decimos hacia donde se va a mover. con smooth hacemos que sea fluido y no de golpe.
+    window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
+  }
+
+  return (
+    <nav className={`navbar-container ${isScrolling > 20 ? 'scrolling' : 0}`}>
+      <div className='navbar-container__logo' onClick={toTop}>Luciano</div>
+    </nav>
+  )
+}
+
+export default Navbar
