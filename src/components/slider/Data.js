c74561ef3b05cc7dc4739811b457a997b8cae85d
Ticiano <ticianomensegue@hotmaiil.com>
2021-09-19 21:51:53 -0300

    fixing projects routes

--- src/components/slider/Data.js
+++ src/components/slider/Data.js
@@ -5,22 +5,22 @@ const slidesData = [
   {
     src: 'https://user-images.githubusercontent.com/83244923/133920153-30181a67-ca6d-4d31-8a24-04f798c58d03.png',
     alt: 'ToDoApp',
-    redirect: 'https://todolist-q3l3h2l10-ticianomen.vercel.app/'
+    redirect: 'https://todolist-ticianomen.vercel.app/'
   },
   {
     src: 'https://user-images.githubusercontent.com/83244923/133920669-409bc435-70e1-4750-b83f-7ea362a384c3.png',
     alt: 'MovieApp',
-    redirect: 'https://movies-sable-eta.vercel.app/'
+    redirect: 'https://movies-ticianomen.vercel.app/'
   },
   {
     src: 'https://user-images.githubusercontent.com/83244923/133920666-b1072ca4-30e1-40f6-912e-d54966b171be.png',
     alt: 'FoodApp',
-    redirect: 'https://pi-food-ft-14b-g4l4ghygu-ticianomen.vercel.app/'
+    redirect: 'https://pi-food-ft-14b-ticianomen.vercel.app/'
   },
   {
     src: 'https://user-images.githubusercontent.com/83244923/133920670-59434d7b-0ca1-4c0c-89fc-2e51b03c6fe0.png',
     alt: 'WeatherApp',
-    redirect: 'https://wheather-app-3wccr5wii-ticianomen.vercel.app/'
+    redirect: 'https://wheather-app-ticianomen.vercel.app/'
   }
 
 ]
