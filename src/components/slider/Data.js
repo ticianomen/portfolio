8f582342948cfa15bda80dedbe45e52efd4bee00
Ticiano <ticianomensegue@hotmaiil.com>
2021-09-22 02:13:26 -0300

    added buttons to projects

--- src/components/slider/Data.js
+++ src/components/slider/Data.js
@@ -4,36 +4,51 @@ import './Slider.css';
 const slidesData = [
   {
     src: 'https://user-images.githubusercontent.com/83244923/133920153-30181a67-ca6d-4d31-8a24-04f798c58d03.png',
+    description:'To Do App, List and track your chores',
     alt: 'ToDoApp',
-    redirect: 'https://todolist-ticianomen.vercel.app/'
+    demo: 'https://todolist-ticianomen.vercel.app/',
+    repo: 'https://github.com/ticianomen/todolist',
   },
   {
     src: 'https://user-images.githubusercontent.com/83244923/133920669-409bc435-70e1-4750-b83f-7ea362a384c3.png',
+    description:'Movies App, List and track your chores',
     alt: 'MovieApp',
-    redirect: 'https://movies-ticianomen.vercel.app/'
+    demo: 'https://movies-ticianomen.vercel.app/',
+    repo: 'https://github.com/ticianomen/Movies',
   },
   {
     src: 'https://user-images.githubusercontent.com/83244923/133920666-b1072ca4-30e1-40f6-912e-d54966b171be.png',
+    description:'Food App, Search and add your recipes',
     alt: 'FoodApp',
-    redirect: 'https://pi-food-ft-14b-ticianomen.vercel.app/'
+    demo: 'https://pi-food-ft-14b-ticianomen.vercel.app/',
+    repo: 'https://github.com/ticianomen/PI-Food-FT14b',
   },
   {
     src: 'https://user-images.githubusercontent.com/83244923/133920670-59434d7b-0ca1-4c0c-89fc-2e51b03c6fe0.png',
+    description:'Weather App, Search weather in different cities',
     alt: 'WeatherApp',
-    redirect: 'https://wheather-app-ticianomen.vercel.app/'
+    demo: 'https://wheather-app-ticianomen.vercel.app/',
+    repo: 'https://github.com/ticianomen/wheatherApp',
   }
-
 ]
 
 const slides = slidesData.map((slide,index) => (
 
   <div className='slide-container' key ={index}>
-    <a href={slide.redirect} target='_blank' rel='noreferrer'>
+      <h2>{slide.alt}</h2>
       <img src={slide.src} alt={slide.alt} />
       <div className='slide-container__desc'>
-        <span>{slide.alt}</span>
+      <div style={{display:'flex',justifyContent:'space-evenly'}}>
+        <a href={slide.demo} target='_blank' rel='noreferrer' >
+          <button>Demo</button>
+        </a>
+        <a href={slide.repo} target='_blank' rel='noreferrer' >
+          <button>Repo</button>
+        </a>
       </div>
-    </a>
+      
+      <span>{slide.description}</span>
+      </div>
   </div>
 
 ))
--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -1,29 +1,50 @@
 import React from 'react'
-import Carousel, { slidesToShowPlugin } from '@brainhubeu/react-carousel'; //importamos el carousel
+import Carousel, { slidesToShowPlugin,arrowsPlugin } from '@brainhubeu/react-carousel'; //importamos el carousel
 import '@brainhubeu/react-carousel/lib/style.css'; //importamos el css que viene instalado en el paquete
+import { Icon } from '@iconify/react'
+
 import Data from './Data';
 import './Slider.css'
-
 const Slider = () => {
   return (
     <div className='carousel-container'>
       <div className='carousel-container__title'>
         <h2>My projects</h2>
       </div>
+     
       <Carousel
-        plugins={[
-          'arrows',
+        
+        plugins={['centered',
+          {
+            resolve: arrowsPlugin,
+            options: {
+              arrowLeft: <button className="icon-button"><Icon icon="ion:chevron-back-outline" width="40"/></button>,
+              arrowLeftDisabled:<button style={{display:'none'}}></button>,
+              arrowRight: <button className="icon-button"><Icon icon="ion:chevron-forward-outline" width="40" /></button>,
+              arrowRightDisabled: <button style={{display:'none'}}></button>,
+              addArrowClickHandler: true,
+            }
+          },
           {
             resolve: slidesToShowPlugin,
             options: {
-              numberOfSlides: 3,
+              numberOfSlides: 2,
             }
           },
         ]}
         breakpoints={{
           960: {
-            plugins: [
-              'arrows',
+            plugins: ['centered',
+              {
+            resolve: arrowsPlugin,
+            options: {
+              arrowLeft: <button className="icon-button"><Icon icon="ion:chevron-back-outline" width="40"  /></button>,
+              arrowLeftDisabled:<button style={{display:'none'}}></button>,
+              arrowRight: <button className="icon-button"><Icon icon="ion:chevron-forward-outline" width="40" /></button>,
+              arrowRightDisabled: <button style={{display:'none'}}></button>,
+              addArrowClickHandler: true,
+            }
+          },
               {
                 resolve: slidesToShowPlugin,
                 options: {
